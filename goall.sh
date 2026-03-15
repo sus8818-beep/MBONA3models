f01726c2444b2ba7cb88688958eb0ba7ba361835
yamauchi89 <yamauchi@cs.chubu.ac.jp>
2026-03-16 01:43:15 +0900

    modified and results

--- goall.sh
+++ goall.sh
@@ -13,21 +13,21 @@ done
 for ((n=1; n<10; n++)); do
     ./run-shrink-sweep.sh -e mainWorker4model1.py -p ./config.yaml -d $DATASETDIR/data0.$n-0.5 -n 50 -r $RESULTDIR/model1result0.$n-0.5 -pr 0.0001
     cd $RESULTDIR
-    tar -czvf results-model2-0.$n-0.5-prth0.0001.tgz *.dat
+    tar -czvf results-model1-0.$n-0.5-prth0.0001.tgz *.dat
     rm *.dat
     cd -
 done
 for ((n=1; n<10; n++)); do
     ./run-shrink-sweep.sh -e mainWorker4model1.py -p ./config.yaml -d $DATASETDIR/data0.$n-0.5 -n 50 -r $RESULTDIR/model1result0.$n-0.5 -pr 0.001
     cd $RESULTDIR
-    tar -czvf results-model2-0.$n-0.5-prth0.001.tgz *.dat
+    tar -czvf results-model1-0.$n-0.5-prth0.001.tgz *.dat
     rm *.dat
     cd -
 done
 for ((n=1; n<10; n++)); do
     ./run-shrink-sweep.sh -e mainWorker4model1.py -p ./config.yaml -d $DATASETDIR/data0.$n-0.5 -n 50 -r $RESULTDIR/model1result0.$n-0.5 -pr 0.01
     cd $RESULTDIR
-    tar -czvf results-model2-0.$n-0.5-prth0.01.tgz *.dat
+    tar -czvf results-model1-0.$n-0.5-prth0.01.tgz *.dat
     rm *.dat
     cd -
 done
