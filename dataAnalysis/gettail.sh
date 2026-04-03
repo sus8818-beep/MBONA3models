2706341f497476def0734b52bb8119f0ae15b204
sus8818-beep <sus8818-beep@gs4e.chubu.ac.jp>
2026-04-04 00:02:05 +0900

    modified

--- dataAnalysis/gettail.sh
+++ dataAnalysis/gettail.sh
@@ -2,18 +2,27 @@
 # This shell script pick up the last cumulative error of each file and unified into combined_LRU?.sh
 # combined_LRU?.sh shows differences between model2 vs model3 in cumulative errors.
 # You should customise RESULTDIR in line 6 if you need it.
+# Usage  ./gettail 
+#        ./gettail 0.999
+#
 #!/bin/sh
 RESULTDIR=../results
+SPONTANEOUS_SHRINKRATIO=$1
+if [ -z $SPONTANEOUS_SHRINKRATIO ]; then
+    echo "Usage ./gettail.sh <spontaneous shrink ratio>"
+    exit 1
+fi
+
 cd $RESULTDIR
 for p in $(seq 1 9)
 do
-   for i in $(seq 50 70)
+   for i in $(seq 60 70)
    do
       for k in $(seq 0 49)
       do
         RATIO=$(awk -v v="$i" 'BEGIN{printf "%.2f", 0.1+v/100}')
         yes | tar -xvf results-model2-0.$p-0.5.tgz model2result0.$p-0.5-$RATIO-$k.dat
-        yes | tar -xzvf results-model3-0.$p-0.5-sp0.999.tgz model3result0.$p-0.5-$RATIO-$k.dat
+        yes | tar -xzvf results-model3-0.$p-0.5-sp$SPONTANEOUS_SHRINKRATIO.tgz model3result0.$p-0.5-$RATIO-$k.dat
         if [ $i -eq 60 ]; then
            if [ $k -eq 0 ]; then
                tail -n 1 model2result0.$p-0.5-$RATIO-$k.dat | cut -d' ' -f2- > forgetron$p.csv
--- goall4model3.sh
+++ goall4model3.sh
@@ -2,10 +2,13 @@ RESULTDIR=./results
 DATASETDIR=./dataset
 SYSTEMDIR=./
 ###### for model3 #######
-for ((n=1; n<10; n++)); do
-    ./run-shrink-sweep.sh -e mainWorker4model3.py -p ./config.yaml -d $DATASETDIR/data0.$n-0.5 -n 50 -r $RESULTDIR/model3result0.$n-0.5 -sp 0.999
-    cd $RESULTDIR
-    tar -czvf results-model3-0.$n-0.5-sp0.999.tgz *.dat
-    rm *.dat
-    cd -
+#
+for sp_ratio in 0.99 0.999 0.9999; do
+    for ((n=1; n<10; n++)); do
+        ./run-shrink-sweep.sh -e mainWorker4model3.py -p ./config.yaml -d $DATASETDIR/data0.$n-0.5 -n 50 -r $RESULTDIR/model3result0.$n-0.5 -sp $sp_ratio
+        cd $RESULTDIR
+        tar -czvf results-model3-0.$n-0.5-sp$sp_ratio.tgz *.dat
+        rm *.dat
+        cd -
+    done
 done
